6156e4666967f7ff7fab183397d351e7ba10c59f
Dennis Funke <dennis.funke@uni-duesseldorf.de>
2012-05-16 10:25:20 +0200

    Main Menu added

--- DynamiteBoy/src/de/propra12/gruppe04/dynamiteboy/Main/Main.java
+++ DynamiteBoy/src/de/propra12/gruppe04/dynamiteboy/Main/Main.java
@@ -1,11 +1,17 @@
 package de.propra12.gruppe04.dynamiteboy.Main;
 import javax.swing.JFrame;
 
+import org.junit.Before;
+
 import de.propra12.gruppe04.dynamiteboy.Game.Game;
+import de.propra12.gruppe04.dynamiteboy.Map.Map;
+import de.propra12.gruppe04.dynamiteboy.Menu.*;
 
 public class Main extends JFrame {
 
 	public Main() {
+
+		add(new MainMenu(640,480));
 		add(new Game());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(640, 480);
