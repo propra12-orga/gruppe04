4c5d6bafa87b4edf5c56e24d04554f73e857d729
Cynthia Maushagen <c.maushagen@hotmail.com>
2012-05-18 01:42:48 +0200

    Fenstergröße an Spielfeld angepasst.

--- DynamiteBoy/src/de/propra12/gruppe04/dynamiteboy/Main/Main.java
+++ DynamiteBoy/src/de/propra12/gruppe04/dynamiteboy/Main/Main.java
@@ -1,20 +1,18 @@
 package de.propra12.gruppe04.dynamiteboy.Main;
+
 import javax.swing.JFrame;
 
-import org.junit.Before;
-
 import de.propra12.gruppe04.dynamiteboy.Game.Game;
-import de.propra12.gruppe04.dynamiteboy.Map.Map;
-import de.propra12.gruppe04.dynamiteboy.Menu.*;
+import de.propra12.gruppe04.dynamiteboy.Menu.MainMenu;
 
 public class Main extends JFrame {
 
 	public Main() {
 
-		add(new MainMenu(640,480));
+		add(new MainMenu(640, 480));
 		add(new Game());
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(640, 480);
+		setSize(665, 520);
 		setLocationRelativeTo(null);
 		setTitle("DynamiteBoy");
 		setResizable(false);
