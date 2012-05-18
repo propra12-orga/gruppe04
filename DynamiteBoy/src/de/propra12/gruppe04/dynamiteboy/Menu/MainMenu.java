cd78c5339c0eca900bb0da39193fd7f7fe5ee3b6
Cynthia Maushagen <c.maushagen@hotmail.com>
2012-05-18 02:00:46 +0200

    Fenstergröße vom Game angepasst an Spielfeldgröße.

--- DynamiteBoy/src/de/propra12/gruppe04/dynamiteboy/Menu/MainMenu.java
+++ DynamiteBoy/src/de/propra12/gruppe04/dynamiteboy/Menu/MainMenu.java
@@ -1,4 +1,5 @@
 package de.propra12.gruppe04.dynamiteboy.Menu;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -24,21 +25,21 @@ public class MainMenu extends Menu {
 		this.frameWidth = width;
 		this.frameHeight = height;
 		JFrame frame = new JFrame();
-		
+
 		buttonStart = new JButton("Spiel starten");
 		buttonStart.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
 				buttonStart.setText("Spiel wird gestartet...");
-				
+
 				add(new Game());
 				setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				setSize(640, 480);
+				setSize(665, 520);
 				setLocationRelativeTo(null);
 				setTitle("DynamiteBoy");
 				setResizable(false);
 				setVisible(true);
-				
+
 				// Start Game here
 			}
 
