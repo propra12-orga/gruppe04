b7f68774e3498d8ab720bf614ad1d77402ba1c26
Cynthia Maushagen <c.maushagen@hotmail.com>
2012-05-18 02:02:43 +0200

    Aufruf von Game nach Aufruf von Mainmenü führte zu einem Fehler, daher auskommentiert. Funktionalität bleibt gleich.

--- DynamiteBoy/src/de/propra12/gruppe04/dynamiteboy/Main/Main.java
+++ DynamiteBoy/src/de/propra12/gruppe04/dynamiteboy/Main/Main.java
@@ -2,7 +2,6 @@ package de.propra12.gruppe04.dynamiteboy.Main;
 
 import javax.swing.JFrame;
 
-import de.propra12.gruppe04.dynamiteboy.Game.Game;
 import de.propra12.gruppe04.dynamiteboy.Menu.MainMenu;
 
 public class Main extends JFrame {
@@ -10,13 +9,12 @@ public class Main extends JFrame {
 	public Main() {
 
 		add(new MainMenu(640, 480));
-		add(new Game());
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setSize(665, 520);
-		setLocationRelativeTo(null);
-		setTitle("DynamiteBoy");
-		setResizable(false);
-		setVisible(true);
+		/*
+		 * add(new Game()); setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		 * 
+		 * setLocationRelativeTo(null); setTitle("DynamiteBoy");
+		 * setResizable(false); setVisible(true);
+		 */
 	}
 
 	public static void main(String[] args) {
