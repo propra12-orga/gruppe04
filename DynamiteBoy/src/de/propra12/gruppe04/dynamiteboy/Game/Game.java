219a6183e087fac4587b713998188e455fbcc5f3
Cynthia Maushagen <c.maushagen@hotmail.com>
2012-06-01 00:46:07 +0200

    Spieler werden dynamisch hinzugefügt durch ein Array, aber Tastatur und Startposition noch nicht.

--- DynamiteBoy/src/de/propra12/gruppe04/dynamiteboy/Game/Game.java
+++ DynamiteBoy/src/de/propra12/gruppe04/dynamiteboy/Game/Game.java
@@ -17,20 +17,26 @@ import de.propra12.gruppe04.dynamiteboy.Map.Map;
 import de.propra12.gruppe04.dynamiteboy.Menu.ScoreMenu;
 
 public class Game extends JPanel {
-	private Player player1;
-	private Player player2;
+	private int playerStartPos[][] = new int[2][2];
+	private Player player[] = new Player[2];
 	private Map map;
 	private JFrame frame;
 	private int numberOfPlayers;
 
-	public Game(JFrame frame) {
+	public Game(JFrame frame, int numberOfPlayers) {
 		// SET UP
+		this.numberOfPlayers = numberOfPlayers;
 		this.map = new Map(640, 480,
 				"src/de/propra12/gruppe04/dynamiteboy/Map/Map1.xml");
 		// this.map = new Map(640, 480);
 		this.frame = frame;
-		numberOfPlayers = 1;
-		createPlayers(numberOfPlayers);
+		this.playerStartPos[0][0] = 32;
+		this.playerStartPos[0][1] = 32;
+		this.playerStartPos[1][0] = 581;
+		this.playerStartPos[1][1] = 416;
+
+		System.err.println(this.numberOfPlayers);
+		createPlayers(this.numberOfPlayers);
 		setFocusable(true);
 		this.addKeyListener(new KAdapter());
 	}
@@ -39,26 +45,27 @@ public class Game extends JPanel {
 		return map;
 	}
 
-	public Player getPlayer() {
-		return player1;
+	public Player getPlayer(int playerIndex) {
+		return this.player[playerIndex];
 	}
 
-	public void createPlayers(int numberOfPĺayers) {
-		if (numberOfPlayers == 1) {
-			this.player1 = new Player(32, 32);
-		} else if (numberOfPlayers == 2) {
-			this.player1 = new Player(32, 32);
-			this.player2 = new Player(128, 128);
+	public void createPlayers(int numberOfPlayers) {
+		for (int i = 0; i < numberOfPlayers; i++) {
+			this.player[i] = new Player(i, this.playerStartPos[i][0],
+					this.playerStartPos[i][1]);
 		}
-
 	}
 
 	/**
 	 * Plants a bomb on current grid-position
 	 */
-	public void plantBomb() {
-		Bomb bomb = new Bomb(player1.getGridX(player1.getxPos() + 16),
-				player1.getGridY(player1.getyPos() + 16), false, map);
+
+	public void plantBomb(int playerIndex) {
+		Bomb bomb = new Bomb(
+				this.player[playerIndex].getGridX(this.player[playerIndex]
+						.getxPos() + 16),
+				this.player[playerIndex].getGridY(this.player[playerIndex]
+						.getyPos() + 16), false, map);
 		Thread bombThread = new Thread(bomb);
 		bombThread.start();
 
@@ -69,12 +76,15 @@ public class Game extends JPanel {
 	private class KAdapter extends KeyAdapter {
 		public void keyReleased(KeyEvent e) {
 			player1KeyReleased(e);
-			player1.move();
+			if (numberOfPlayers > 1)
+				player2KeyReleased(e);
 		}
 
 		public void keyPressed(KeyEvent e) {
+			int playerSum = player.length;
 			player1KeyPressed(e);
-			player1.move();
+			if (numberOfPlayers > 1)
+				player2KeyPressed(e);
 		}
 	}
 
@@ -88,16 +98,21 @@ public class Game extends JPanel {
 		}
 	}
 
-	public void paintPlayer(Graphics g2d) {
-		g2d.drawImage(player1.getImage(), player1.getxPos(), player1.getyPos(),
-				this);
+	public void paintPlayer(Graphics g2d, int playerIndex) {
+		g2d.drawImage(this.player[playerIndex].getImage(),
+				this.player[playerIndex].getxPos(),
+				this.player[playerIndex].getyPos(), this);
 	}
 
 	public void paint(Graphics g) {
 		super.paint(g);
 		Graphics g2d = (Graphics2D) g;
 		paintField(g2d);
-		paintPlayer(g2d);
+
+		for (int i = 0; i < this.numberOfPlayers; i++) {
+			paintPlayer(g2d, i);
+		}
+
 		Toolkit.getDefaultToolkit().sync();
 		g.dispose();
 		// Redraw with 30fps
@@ -115,57 +130,109 @@ public class Game extends JPanel {
 	 *            takes key event (pressed) and changes player-position
 	 *            accordingly
 	 */
+	public void playerMoveLeft(int playerIndex) {
+		if (map.getFieldByPixel(this.player[playerIndex].getxPos(),
+				this.player[playerIndex].getyPos()).isBlocked() == false
+				&& map.getFieldByPixel(this.player[playerIndex].getxPos(),
+						this.player[playerIndex].getyPos() + 30).isBlocked() == false) {
+			this.itemHandling(this.player[playerIndex].getxPos(),
+					this.player[playerIndex].getyPos());
+			this.player[playerIndex].setDx(-4);
+			this.player[playerIndex].setDy(0);
+		}
+	}
+
+	public void playerMoveRight(int playerIndex) {
+		if (map.getFieldByPixel(this.player[playerIndex].getxPos() + 28,
+				this.player[playerIndex].getyPos()).isBlocked() == false
+				&& map.getFieldByPixel(this.player[playerIndex].getxPos() + 28,
+						this.player[playerIndex].getyPos() + 28).isBlocked() == false) {
+			this.itemHandling(this.player[playerIndex].getxPos(),
+					this.player[playerIndex].getyPos());
+			this.player[playerIndex].setDx(4);
+			this.player[playerIndex].setDy(0);
+		}
+	}
+
+	public void playerMoveUp(int playerIndex) {
+
+		if (map.getFieldByPixel(this.player[playerIndex].getxPos(),
+				this.player[playerIndex].getyPos()).isBlocked() == false
+				&& map.getFieldByPixel(this.player[playerIndex].getxPos() + 22,
+						this.player[playerIndex].getyPos()).isBlocked() == false) {
+			this.itemHandling(this.player[playerIndex].getxPos(),
+					this.player[playerIndex].getyPos());
+			this.player[playerIndex].setDy(-4);
+			this.player[playerIndex].setDx(0);
+		}
+	}
+
+	public void playerMoveDown(int playerIndex) {
+		if (map.getFieldByPixel(this.player[playerIndex].getxPos(),
+				this.player[playerIndex].getyPos() + 30).isBlocked() == false
+				&& map.getFieldByPixel(this.player[playerIndex].getxPos() + 22,
+						this.player[playerIndex].getyPos() + 32).isBlocked() == false) {
+			this.itemHandling(this.player[playerIndex].getxPos(),
+					this.player[playerIndex].getyPos());
+			this.player[playerIndex].setDy(4);
+			this.player[playerIndex].setDx(0);
+		}
+	}
 
 	public void player1KeyPressed(KeyEvent e) {
 
 		int key = e.getKeyCode();
 
 		if (key == KeyEvent.VK_LEFT) {
-			if (map.getFieldByPixel(player1.getxPos(), player1.getyPos())
-					.isBlocked() == false
-					&& map.getFieldByPixel(player1.getxPos(),
-							player1.getyPos() + 30).isBlocked() == false) {
-				this.itemHandling(player1.getxPos(), player1.getyPos());
-				player1.setDx(-4);
-				player1.setDy(0);
-			}
+			playerMoveLeft(0);
 		}
 
 		if (key == KeyEvent.VK_RIGHT) {
-			if (map.getFieldByPixel(player1.getxPos() + 28, player1.getyPos())
-					.isBlocked() == false
-					&& map.getFieldByPixel(player1.getxPos() + 28,
-							player1.getyPos() + 28).isBlocked() == false) {
-				this.itemHandling(player1.getxPos(), player1.getyPos());
-				player1.setDx(4);
-				player1.setDy(0);
-			}
+			playerMoveRight(0);
 		}
 
 		if (key == KeyEvent.VK_UP) {
-			if (map.getFieldByPixel(player1.getxPos(), player1.getyPos())
-					.isBlocked() == false
-					&& map.getFieldByPixel(player1.getxPos() + 22,
-							player1.getyPos()).isBlocked() == false) {
-				this.itemHandling(player1.getxPos(), player1.getyPos());
-				player1.setDy(-4);
-				player1.setDx(0);
-			}
+			playerMoveUp(0);
 		}
 
 		if (key == KeyEvent.VK_DOWN) {
-			if (map.getFieldByPixel(player1.getxPos(), player1.getyPos() + 30)
-					.isBlocked() == false
-					&& map.getFieldByPixel(player1.getxPos() + 22,
-							player1.getyPos() + 32).isBlocked() == false) {
-				this.itemHandling(player1.getxPos(), player1.getyPos());
-				player1.setDy(4);
-				player1.setDx(0);
-			}
+			playerMoveDown(0);
 		}
+
+		if (key == KeyEvent.VK_ENTER) {
+			plantBomb(0);
+		}
+
+		this.player[0].move();
+
+	}
+
+	public void player2KeyPressed(KeyEvent e) {
+
+		int key = e.getKeyCode();
+
+		if (key == KeyEvent.VK_A) {
+			playerMoveLeft(1);
+		}
+
+		if (key == KeyEvent.VK_D) {
+			playerMoveRight(1);
+		}
+
+		if (key == KeyEvent.VK_W) {
+			playerMoveUp(1);
+		}
+
+		if (key == KeyEvent.VK_S) {
+			playerMoveDown(1);
+		}
+
 		if (key == KeyEvent.VK_SPACE) {
-			plantBomb();
+			plantBomb(1);
 		}
+
+		this.player[1].move();
+
 	}
 
 	/**
@@ -177,20 +244,42 @@ public class Game extends JPanel {
 		int key = e.getKeyCode();
 
 		if (key == KeyEvent.VK_LEFT) {
-			player1.setDx(0);
+			this.player[0].setDx(0);
 		}
 
 		if (key == KeyEvent.VK_RIGHT) {
-			player1.setDx(0);
+			this.player[0].setDx(0);
 		}
 
 		if (key == KeyEvent.VK_UP) {
-			player1.setDy(0);
+			this.player[0].setDy(0);
 		}
 
 		if (key == KeyEvent.VK_DOWN) {
-			player1.setDy(0);
+			this.player[0].setDy(0);
 		}
+
+	}
+
+	public void player2KeyReleased(KeyEvent e) {
+		int key = e.getKeyCode();
+
+		if (key == KeyEvent.VK_A) {
+			this.player[1].setDx(0);
+		}
+
+		if (key == KeyEvent.VK_D) {
+			this.player[1].setDx(0);
+		}
+
+		if (key == KeyEvent.VK_W) {
+			this.player[1].setDy(0);
+		}
+
+		if (key == KeyEvent.VK_S) {
+			this.player[1].setDy(0);
+		}
+
 	}
 
 	/**
