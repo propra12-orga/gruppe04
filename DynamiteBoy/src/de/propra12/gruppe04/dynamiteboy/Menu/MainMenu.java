f15d2725246414a9bc864b29dbcc6e3e5cba3c42
Cynthia Maushagen <c.maushagen@hotmail.com>
2012-06-01 00:44:23 +0200

    Zweiten Button hinzugefügt für Zweispielermodus.

--- DynamiteBoy/src/de/propra12/gruppe04/dynamiteboy/Menu/MainMenu.java
+++ DynamiteBoy/src/de/propra12/gruppe04/dynamiteboy/Menu/MainMenu.java
@@ -3,6 +3,7 @@ package de.propra12.gruppe04.dynamiteboy.Menu;
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.Graphics;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -14,9 +15,14 @@ import javax.swing.JPanel;
 import de.propra12.gruppe04.dynamiteboy.Game.Game;
 
 public class MainMenu extends JPanel {
-	private JButton buttonStart;
+	private JButton ButtonStart1p;
+	private JButton ButtonStart2p;
 	private JFrame frame;
 	private TitlePanel title = new TitlePanel();
+
+	private JPanel panelButton = new JPanel(new GridLayout(1, 2)); // panelButton
+																	// das
+																	// GridLayout
 
 	/**
 	 * Constructor Sets up MainMenu with a Start Button to start the game
@@ -26,25 +32,45 @@ public class MainMenu extends JPanel {
 	 * @param height
 	 *            Window-height
 	 */
+
+	public void loadGame(int numberOfPlayers) {
+		Game game = new Game(frame, numberOfPlayers);
+		frame.getContentPane().add(game);
+		title.setVisible(false);
+		panelButton.setVisible(false);
+		game.setVisible(true);
+	}
+
 	public MainMenu(final JFrame frame) {
 		this.frame = frame;
 		// final JFrame frame = new JFrame();
-		buttonStart = new JButton("Spiel starten");
-		buttonStart.addActionListener(new ActionListener() {
+		ButtonStart1p = new JButton("1 Spieler");
+		ButtonStart2p = new JButton("2 Spieler");
+		panelButton.add(ButtonStart1p);
+		panelButton.add(ButtonStart2p);
+
+		ButtonStart1p.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				buttonStart.setText("Spiel wird gestartet...");
-				Game game = new Game(frame);
-				frame.getContentPane().add(game);
-				buttonStart.setVisible(false);
-				title.setVisible(false);
-				game.setVisible(true);
+				ButtonStart1p.setText("Spiel wird gestartet...");
+				loadGame(1);
 			}
 
 		});
+
+		ButtonStart2p.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				ButtonStart2p.setText("Spiel wird gestartet...");
+				loadGame(2);
+			}
+
+		});
+
 		frame.getContentPane().add(BorderLayout.CENTER, this.title);
-		buttonStart.setPreferredSize(new Dimension(100, 80));
-		frame.getContentPane().add(BorderLayout.SOUTH, buttonStart);
+		ButtonStart1p.setPreferredSize(new Dimension(100, 80));
+		ButtonStart2p.setPreferredSize(new Dimension(100, 80));
+		frame.getContentPane().add(BorderLayout.SOUTH, panelButton);
 	}
 
 	static class TitlePanel extends JPanel {
