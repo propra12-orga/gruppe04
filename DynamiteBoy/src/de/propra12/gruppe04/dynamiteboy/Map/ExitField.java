50af228c4716dde0877eb98c9445189f7ca2ab8d
Cynthia Maushagen <c.maushagen@hotmail.com>
2012-06-04 00:25:15 +0200

    Exitfield hinzugefügt und eingesetzt

--- DynamiteBoy/src/de/propra12/gruppe04/dynamiteboy/Map/ExitField.java
+++ DynamiteBoy/src/de/propra12/gruppe04/dynamiteboy/Map/ExitField.java
@@ -4,7 +4,7 @@ import de.propra12.gruppe04.dynamiteboy.Item.Exit;
 import de.propra12.gruppe04.dynamiteboy.Item.Item;
 
 public class ExitField extends Field {
-	private static final String DEFAULT_PIC = "../images/db_field_exit.png";
+	private static final String DEFAULT_PIC = "../images/db_field_exit.gif";
 	private static final boolean DEFAULT_BLOCKED = false;
 	private static final boolean DEAFULT_DESTROYABLE = false;
 	private static final Item DEFAULT_ITEM = new Exit(false);
