3db76a899335917a8d9d936dee78f366f81992f7
Jan-Erik Bähr <jan-erik.baehr@hhu.de>
2012-06-25 19:03:46 +0200

    Worked on editor, GUI now almost complete

--- DynamiteBoy/src/de/propra12/gruppe04/dynamiteboy/Map/Editor.java
+++ DynamiteBoy/src/de/propra12/gruppe04/dynamiteboy/Map/Editor.java
@@ -1,9 +1,7 @@
 package de.propra12.gruppe04.dynamiteboy.Map;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Dimension;
-import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.GridLayout;
@@ -12,18 +10,32 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
+import java.util.Enumeration;
 
+import javax.swing.AbstractButton;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 
 import de.propra12.gruppe04.dynamiteboy.Game.InputHandler;
 
+/**
+ * This class represents the Map Editor
+ * 
+ * @author ekki
+ * 
+ */
 public class Editor extends JPanel implements MouseListener {
-	JFrame frame;
+	private JFrame frame;
 	private boolean running = true;
-	Map map;
-	InputHandler input;
+	private Map map;
+	private String mapname;
+	private String authorname;
+	private String mapType;
+	private InputHandler input;
 	private JButton buttonChangeFieldType;
 	private JButton buttonResetSelection;
 	private JButton buttonSaveAndExit;
@@ -31,14 +43,48 @@ public class Editor extends JPanel implements MouseListener {
 	private Boolean selection = false;
 	private String oldpicture;
 	private Field f = null;
+	private ButtonGroup buttonGroupFieldType;
+	private JRadioButton radioButtonFloorField;
+	private JRadioButton radioButtonWallField;
+	private JRadioButton radioButtonDestroyableField;
+	private ButtonGroup buttonGroupItemType;
+	private JRadioButton radioButtonExitItem;
 
-	public Editor(JFrame frame) {
+	// Add new items here to the editor
+	// TODO find a way to handle added items dynamically (build a resource
+	// manager that supplies the editor and the game with new items, maybe via
+	// some sort of "entity model")
+
+	/**
+	 * Starts a new Editor with the given parameters
+	 * 
+	 * @param frame
+	 *            The frame the editor should run in
+	 * @param mapname
+	 *            name of the map to generate
+	 * @param authorname
+	 *            map creator name
+	 * @param mapType
+	 *            choose between singleplayer or multiplayer map
+	 */
+	public Editor(JFrame frame, String mapname, String authorname,
+			String mapType) {
+		// TODO: generate map-object and change mapname-element,author-element
+		// and maptype-element
+		// TODO map needs saveMap-method that saves dom into xml-file, name
+		// based on mapname-element
 		this.map = new Map(640, 480, "MapTemplate.xml");
+		this.mapname = mapname;
+		this.authorname = authorname;
+		this.mapType = mapType;
 		this.frame = frame;
-		setFocusable(true);
+		this.setFocusable(true);
 		addMouseListener(this);
 		initGUI();
 		repaint();
+		// TODO REMOVE DEBUG
+		System.out.println("Started editor with the following settings: \n"
+				+ mapname + ", " + authorname + ", " + mapType);
 	}
 
 	public void initGUI() {
@@ -51,6 +97,12 @@ public class Editor extends JPanel implements MouseListener {
 		buttonChangeFieldType.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				String fieldType = getFieldTypeDecision();
+				if (fieldType == "destroyable") {
+					String itemType = getItemTypeDecision();
+				}
+				// TODO Save FieldType and ItemType to DOM-Object (has to get
+				// coordinates from somewhere, too)
 			}
 		});
 		buttonResetSelection.addActionListener(new ActionListener() {
@@ -68,6 +120,62 @@ public class Editor extends JPanel implements MouseListener {
 		buttonResetSelection.setPreferredSize(new Dimension(60, 32));
 		buttonSaveAndExit.setPreferredSize(new Dimension(60, 32));
 		frame.getContentPane().add(BorderLayout.SOUTH, panelEdit);
+	}
+
+	public String getSelectedButton(ButtonGroup group) {
+		Enumeration<AbstractButton> e = group.getElements();
+		while (e.hasMoreElements()) {
+			AbstractButton b = e.nextElement();
+			if (b.isSelected())
+				return b.getText();
+		}
+		return null;
+	}
+
+	/**
+	 * returns
+	 * 
+	 * Field Types: (1) "": empty string represents FloorField (2) "wall":
+	 * WallField (3) "destroyable": DestroyableField
+	 * 
+	 * @return
+	 */
+	public String getFieldTypeDecision() {
+		// DEFAULT FIELD TYPE GETS SET HERE
+		// TODO create constant
+		String fieldType = "";
+		Object[] fieldTypes = { "Boden", "Wand", "Zerstörbare Wand" };
+		fieldType = (String) JOptionPane.showInputDialog(frame,
+				"Bitte Feldtyp wählen: \n", "Feldtyp",
+				JOptionPane.PLAIN_MESSAGE, null, fieldTypes, "");
+		if ((fieldType != null) && (fieldType.length() > 0)) {
+			if (fieldType.equals(fieldTypes[0])) {
+				fieldType = "";
+				return fieldType;
+			}
+			if (fieldType.equals(fieldTypes[1])) {
+				fieldType = "wall";
+				return fieldType;
+			}
+			if (fieldType.equals(fieldTypes[2])) {
+				fieldType = "destroyable";
+				return fieldType;
+			}
+		}
+		return fieldType;
+	}
+
+	public String getItemTypeDecision() {
+		// TODO write method that checks whick radiobutton for item type of
+		// wallfield has been chosen
+		// and assigns it to the dom-object
+		// TODO change return
+		return authorname;
+	}
+
+	public boolean isMapValid(Map map) {
+		// TODO write method that checks map vor validity
+		return true;
 	}
 
 	public void resetSelectedField() {
@@ -100,10 +208,6 @@ public class Editor extends JPanel implements MouseListener {
 						.getImage(), x, y, this);
 			}
 		}
-		g2d.setColor(Color.black);
-		Font font = new Font("Ubuntu", Font.BOLD, 12);
-		g2d.setFont(font);
-		g2d.drawString("Time: ", 5, 492);
 		Toolkit.getDefaultToolkit().sync();
 		g.dispose();
 	}
