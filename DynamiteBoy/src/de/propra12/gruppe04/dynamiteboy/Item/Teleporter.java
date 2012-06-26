12d34bde52ed96fd2e2a0ef421cc653cd86d7522
Peter Mösenthin <peter.moesenthin@googlemail.com>
2012-06-26 12:43:30 +0200

    teleporter works fine now

--- DynamiteBoy/src/de/propra12/gruppe04/dynamiteboy/Item/Teleporter.java
+++ DynamiteBoy/src/de/propra12/gruppe04/dynamiteboy/Item/Teleporter.java
@@ -17,14 +17,14 @@ public class Teleporter extends Item {
 	 * @param map
 	 */
 	public void teleport(Player player, Map map) {
-		int teleX = (int) Math.random() * 20;
-		int teleY = (int) Math.random() * 15;
+		int teleX = (int) (Math.random() * 20);
+		int teleY = (int) (Math.random() * 15);
 		System.out.println("Teleport to " + teleX + "/" + teleY);
 		if (map.getField(teleX, teleY) instanceof FloorField) {
 			player.setxPos(teleX * 32);
 			player.setyPos(teleY * 32);
 		} else {
-
+			teleport(player, map);
 		}
 
 	}
